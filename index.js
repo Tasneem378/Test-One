f39d73f1fd4c7fa9602b89d4e34e24fd26e2af9d
Tasneem378 <tasneem378@gmail.com>
2023-08-24 16:38:08 +0500

    Create JS File

---
+++ index.js
@@ -0,0 +1,11 @@
+// 3-QUESTION.Use forEach to console.log each country in the countries array.
+const countries = [
+    "Finland",
+    "Sweden",
+    "England",
+    "Norway",
+    "Iceland",
+    "Ireland",
+  ];
+  countries.forEach((element, index) => console.log(element, index));
+      console.log(countries);
